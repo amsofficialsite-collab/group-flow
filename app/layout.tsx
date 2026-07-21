803be86c115075ccfef85662bfc27fb8aa5a12b2
amsofficialsite-collab <ams.official.site@gmail.com>
2026-07-21 17:18:39 +0700

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "GROUP FLOW",
-  description: "Facebook Group Content & Posting Management",
+  description: "Facebook Group content and posting management",
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
