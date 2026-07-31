fd4c18ef87ed5feb4feb32a6322e4f7ec0b51d26
amsofficialsite-collab <ams.official.site@gmail.com>
2026-07-31 16:48:01 +0700

    Update config.js

--- chrome-extension/config.js
+++ chrome-extension/config.js
@@ -1,4 +1,4 @@
 self.GROUPFLOW_CONFIG = {
   API_BASE_URL: "https://group-flow-two.vercel.app",
-  AGENT_SECRET: "AMS-GroupFlow-2026-x9LkP82qV7mNs41"
+  AGENT_SECRET: "AMS-GroupFlow-2026-NewSecret-9Kx72Pm4"
 };
